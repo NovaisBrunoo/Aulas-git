9ab5f8b92cfc09f580a6c9625f193cdf4cf23afe
Bruno <novais-bruno@hotmail.com>
2022-09-05 18:46:44 -0300

    Operação Matematicas!

--- index-2.js
+++ index-2.js
@@ -1,1 +1,21 @@
+console.log("Aulas git!");
 
+const x = 10
+const y = 5
+
+let soma = x + y
+console.log(soma);
+console.log("--------------------")
+
+
+let subitração = x - y
+console.log(subitração);
+console.log("---------------------");
+
+let divisão = x / y
+console.log(divisão);
+console.log("---------------------")
+
+let multiplicação = x * y
+console.log(multiplicação);
+console.log("--------------------");
